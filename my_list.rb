f2d7ac8fa3782e4e6d697526d44fca72bd75f5eb
MohammadYaser <yasersafi19@outlook.com>
2023-11-03 21:26:34 +0430

    fix linter error

--- my_list.rb
+++ my_list.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Style/ExplicitBlockArgument
 require_relative 'my_enumerable'
 
 class MyList
@@ -8,6 +9,9 @@ class MyList
   end
 
   def each
-    @list.each { |item| yield(item) }
+    @list.each do |item|
+      yield(item)
+    end
   end
 end
+# rubocop:enable Style/ExplicitBlockArgument
